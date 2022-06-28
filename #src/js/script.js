f5e798bf52c1c92d0875553c803070ac78780f87
TrubitsynEgor <trubitsynwork74@gmail.com>
2022-06-28 18:28:19 +0500

    add burger menu + full adaptive

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,20 +1,21 @@
-
-
-
 function testWebP(callback) {
-
 	var webP = new Image();
 	webP.onload = webP.onerror = function () {
 		callback(webP.height == 2);
 	};
 	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
-
 testWebP(function (support) {
-
 	if (support == true) {
 		document.querySelector('body').classList.add('webp');
 	} else {
 		document.querySelector('body').classList.add('no-webp');
 	}
 });
+
+$(document).ready(function () {
+	$('.burger').click(function (event) {
+		$('.burger,.menu,.icon__link,.logo,.intro__box, .intro__slider, body,.top,.bot,.mid').toggleClass('active');
+		$('body').toggleClass('lock');
+	})
+})
