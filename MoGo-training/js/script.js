b3202470053442dc2c3adba135655315958c1bbc
TrubitsynEgor <trubitsynwork74@gmail.com>
2022-06-30 21:05:42 +0500

    add slader + adaptive (full js)

--- MoGo-training/js/script.js
+++ MoGo-training/js/script.js
@@ -37,3 +37,14 @@ $(document).ready(function () {
 		$('body').toggleClass('lock');
 	})
 })
+
+new Swiper('.slider', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	}
+});
